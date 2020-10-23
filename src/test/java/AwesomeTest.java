8cd5addc9d7f55dfde5791476a3406d8dbb19c3a
Meltem885 <meltemcoban@windowslive.com>
2020-10-23 17:53:03 +0100

    Added more awesome stuff

--- src/test/java/AwesomeTest.java
+++ src/test/java/AwesomeTest.java
@@ -4,5 +4,8 @@ public class AwesomeTest {
     //grey in .idea folder means it's ignored
     //when the color is normal, it means it's commited no changes
 
+// I just pulled the new change from GITHUB that I did not leave locally
+// and now my project is in sync with remote repository
+    //Now I made more awesome changes and I am about to push this back to Github
 
 }
